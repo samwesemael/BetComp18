03ba3240b0df9420dca9cecb2d17ad2b1ec8ada3
swesemae <Sam.Wesemael@UGent.be>
2018-01-08 15:18:33 +0100

    index

--- pages/index.php
+++ pages/index.php
@@ -257,37 +257,39 @@
                 </div>
                 <!-- #END# Answered Tickets -->
          
-			
-				<div>
-                <!-- Visitors -->
+				<!-- Answered Tickets -->
                 <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
                     <div class="card">
-                        <div class="body bg-pink">
-                            <div class="sparkline" data-type="line" data-spot-Radius="4" data-highlight-Spot-Color="rgb(233, 30, 99)" data-highlight-Line-Color="#fff"
-                                 data-min-Spot-Color="rgb(255,255,255)" data-max-Spot-Color="rgb(255,255,255)" data-spot-Color="rgb(255,255,255)"
-                                 data-offset="90" data-width="100%" data-height="92px" data-line-Width="2" data-line-Color="rgba(255,255,255,0.7)"
-                                 data-fill-Color="rgba(0, 188, 212, 0)">
-                                12,10,9,6,5,6,10,5,7,5,12,13,7,12,11
-                            </div>
+                        <div class="body bg">
+                            <div class="font-bold m-b--35">LAATSTE WEDSTRIJDEN</div>
                             <ul class="dashboard-stat-list">
                                 <li>
-                                    TODAY
-                                    <span class="pull-right"><b>1 200</b> <small>USERS</small></span>
+                                    PANAMA - PANAMA
+                                    <span class="pull-right col-blue"><b>0-0</b> <small></small></span>
                                 </li>
                                 <li>
-                                    YESTERDAY
-                                    <span class="pull-right"><b>3 872</b> <small>USERS</small></span>
+                                    PANAMA - PANAMA
+                                    <span class="pull-right col-blue"><b>0-0</b> <small></small></span>
                                 </li>
                                 <li>
-                                    LAST WEEK
-                                    <span class="pull-right"><b>26 582</b> <small>USERS</small></span>
+                                    PANAMA - PANAMA
+                                    <span class="pull-right col-blue"><b>0-0</b> <small></small></span>
+                                </li>
+                                <li>
+                                    PANAMA - PANAMA
+                                    <span class="pull-right col-blue"><b>0-0</b> <small></small></span>
+                                </li>
+                                <li>
+                                    PANAMA - PANAMA
+                                    <span class="pull-right col-blue"><b>GROEPSFASE</b> <small></small></span>
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </div>
-                <!-- #END# Visitors -->
+                <!-- #END# Answered Tickets -->
 			
+		
 			
             <!-- CPU Usage -->
             <div class="row clearfix">
