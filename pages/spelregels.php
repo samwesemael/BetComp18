e8cdd42c42fd96264244c5a21273490eadc29e1f
Jasper Poppe <jasperpoppe@msn.com>
2018-06-14 08:29:16 +0200

    :cop: Whitespace

--- pages/spelregels.php
+++ pages/spelregels.php
@@ -1,119 +1,120 @@
- <h4>
-                                SPELREGELS
-                            </h4>
-						</div>
-    					<div class="body">
-    				        <h4>Wedstrijden</h4>
-    					       <p> We gokken op 1 match per dag zodat de studenten maar 2 uur per dag nagelbijtend voor hun tv moeten zitten. Dit zijn in totaal <b>25 wedstrijden</b>, <br> waarvan de te wedden wedstrijden van de groepsfase voor aanvang van het WK bepaald worden.</p>
-							<h4> Punten </h4>
-    					
-                            <p> Iedereen geeft voor aanvang van de wedstrijd zijn gok voor de einduitslag mee:
-        					    <ul>
-                                    <li> Score volledig juist: <b>&#9;3 punten</b> </li>
-                                    <li> Winnaar/gelijkspel juist: <b>&#9;1 punt</b> </li> 
-                                </ul>
-                               
-								De uitzondering zit hem vanaf de knock-outfase. De score die je dan indient is de score na <b>90'</b>. Wanneer je een gelijkspel indient vul je in het vakje 'extra' 
-								ook de winnaar na <b>120'</b> (verlengingen/penalties) in. Wanneer je dit correct hebt kan je een extra punt verdienen, degenen die geen gelijkspel invulden kunnen 
-								dit punt ook verdienen als zij de winnende ploeg in hun pronotiek juist inschatten.
-								<br>De puntenverdeling is dan als volgt: 
-								<p>
-								<ul>
-									<li>je had het gelijkspel na 120' juist met de juiste score en duidt de verkeerde winnaar aan na 120': <b>3 punten</b> </li>
-									<li>je had het gelijkspel na 120' juist met de juiste score en duidt de juiste winnaar aan na 120': <b>4 punten</b> </li>
-									<li>je had het gelijkspel na 120' juist maar niet de juiste score en duidt de juiste winnaar aan na 120': <b>2 punten</b> </li>
-									<li>je had het gelijkspel na 120' juist maar niet de juiste score en duidt de foute winnaar aan: <b>1 punt</b> </li>
-									<li>je gokt geen gelijkspel, en hebt de score perfect juist: <b>4 punten</b> </li>
-									<li>je gokt geen gelijkspel, en hebt de winnaar juist: <b>2 punten</b> </li>
-								</ul>
-								</p>								
-								Indien er na puntenverdeling van alle wedstrijden een gelijkstand plaatsvindt zijn dit de volgende criteria waarop je positie in het klassement bepaald wordt: 
-                            </p> 
-                            <p>
-                                <ol>
-                                    <li>Aantal maal de score volledig correct tijdens het WK</li>
-                                    <li>Minuut van de eerste goal in de finale</li>             
-                                    </ol> 
-                            </p>
-    								
-    				        <h4> Gokken indienen </h4>
-                            <p> 
-                                De gokken zullen worden ingediend op het tabblad Betting. <br>
-    				           
-        				        <ul> 
-        				            <li> Je kan je gok zoveel aanpassen als je wilt (opnieuw indienen van die speeldag), enkel de laatst ingediende telt </li>
-        				            <li> Er is een timecheck ingebouwd, je kan tot 1 seconde voor aanvang van de wedstrijd posten, als je na aanvang post, wordt je gok door de website genegeerd </li>
-        				        </ul>
-    				        </p>
+<h4>
+    SPELREGELS
+</h4>
+</div>
+<div class="body">
+    <h4>Wedstrijden</h4>
+       <p> We gokken op 1 match per dag zodat de studenten maar 2 uur per dag nagelbijtend voor hun tv moeten zitten. Dit zijn in totaal <b>25 wedstrijden</b>, <br> waarvan de te wedden wedstrijden van de groepsfase voor aanvang van het WK bepaald worden.</p>
+    <h4> Punten </h4>
 
-    				        <h4> Bonussen </h4>
+    <p> Iedereen geeft voor aanvang van de wedstrijd zijn gok voor de einduitslag mee:
+        <ul>
+            <li> Score volledig juist: <b>&#9;3 punten</b> </li>
+            <li> Winnaar/gelijkspel juist: <b>&#9;1 punt</b> </li>
+        </ul>
 
-                            <p> Met de bonussen blijft het spannend tot op het einde, je kan hier tot 10 extra punten mee verdienen.
-                            	Voor de start van het toernooi moet ik van iedereen een voorspelling hebben (via tabblad Gokken --> bonussen) voor onderstaande gebeurtenissen:
+        De uitzondering zit hem vanaf de knock-outfase. De score die je dan indient is de score na <b>90'</b>. Wanneer je een gelijkspel indient vul je in het vakje 'extra'
+        ook de winnaar na <b>120'</b> (verlengingen/penalties) in. Wanneer je dit correct hebt kan je een extra punt verdienen, degenen die geen gelijkspel invulden kunnen
+        dit punt ook verdienen als zij de winnende ploeg in hun pronotiek juist inschatten.
+        <br>De puntenverdeling is dan als volgt:
+        <p>
+        <ul>
+            <li>je had het gelijkspel na 120' juist met de juiste score en duidt de verkeerde winnaar aan na 120': <b>3 punten</b> </li>
+            <li>je had het gelijkspel na 120' juist met de juiste score en duidt de juiste winnaar aan na 120': <b>4 punten</b> </li>
+            <li>je had het gelijkspel na 120' juist maar niet de juiste score en duidt de juiste winnaar aan na 120': <b>2 punten</b> </li>
+            <li>je had het gelijkspel na 120' juist maar niet de juiste score en duidt de foute winnaar aan: <b>1 punt</b> </li>
+            <li>je gokt geen gelijkspel, en hebt de score perfect juist: <b>4 punten</b> </li>
+            <li>je gokt geen gelijkspel, en hebt de winnaar juist: <b>2 punten</b> </li>
+        </ul>
+        </p>
+        Indien er na puntenverdeling van alle wedstrijden een gelijkstand plaatsvindt zijn dit de volgende criteria waarop je positie in het klassement bepaald wordt:
+    </p>
+    <p>
+        <ol>
+            <li>Aantal maal de score volledig correct tijdens het WK</li>
+            <li>Minuut van de eerste goal in de finale</li>
+            </ol>
+    </p>
 
-                                <ul> 
-                                    <li>  Wie wordt wereldkampioen? <b>4</b> punten </li>
-                                    <li>  Wie is verliezend finalist? <b>2</b> punten </li>
-                                    <li> Waar eindigen de Belgen? <b>2</b> punten </li>
-                                    <li>  Wie wordt de vuilste ploeg? <b>2</b> punt</li>
-                                    <li> Wie wordt topschutter? <b>2</b> punt</li> 
-                                </ul>
-                                Als je finalist blijkt kampioen te worden en/of omgekeerd krijg je ook nog een aantal punten:
-								<ul>
-								<li> Wereldkampioen OF finalist juist, maar op de verkeerde positie: <b>2</b> punten </li>
-								<li> Wereldkampioen EN finalist juist, maar omgewisseld: <b>4</b> punten</li>
-								</ul>
-								Bij wereldkampioen en finalist hetzelfde land invullen is verboden.
-                            </p>
-							<br>
-                            <h4> Berekening bonussen </h4>
-							Iedereen met het juiste resultaat ontvangt de bonuspunten! <br>
-                            <p>
-                                <ol>
-                                    <li> Vuilste ploeg:
-                                        <ul> 
-                                            <li> gele kaart = <b>1</b> punt </li>                                         
-                                            <li> rode kaart = <b>3</b> punten </li>
-                                  
-                                        </ul> 
-                                    </li>
-									<br>
-                                    <li>
-                                    Positie België:
-                                        <ul> 
-                                            <li>finale </li>
-                                            <li> halve finale </li>
-                                            <li> kwartfinale </li>
-                                            <li> 1/8ste finale </li>
-                                            <li> groepsfase </li> 
-                                        </ul> <br>
-                                        Je gokt het verst je denkt dat ze geraken, als je dit correct hebt krijg je 2 punten. 
-                                    </li>
-                                </ol>
-                            </p>
-                            <br>
+    <h4> Gokken indienen </h4>
+    <p>
+        De gokken zullen worden ingediend op het tabblad Betting. <br>
 
-                            <h4> Inzet </h4>
+        <ul>
+            <li> Je kan je gok zoveel aanpassen als je wilt (opnieuw indienen van die speeldag), enkel de laatst ingediende telt </li>
+            <li> Er is een timecheck ingebouwd, je kan tot 1 seconde voor aanvang van de wedstrijd posten, als je na aanvang post, wordt je gok door de website genegeerd </li>
+        </ul>
+    </p>
 
-                            <p> Iedereen bezorgt mij 10€ voor 14 juni 2018, dit kan cash of via overschrijving:
-                                <div class="row">
-                                    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-                                        <div class="info-box-4">
-                                            <div class="icon">
-                                                <i class="material-icons col-light-green">credit_card</i>
-                                            </div>
-                                            <div class="content">
-                                                <div class="text"><h4>Sam Wesemael</h4></div>
-                                                <div class="number">BE46 7805 9286 6336</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </p>
-                            <h4> Winst </h4>
+    <h4> Bonussen </h4>
 
-							 Het opgehaalde geld wordt als volgt verdeeld:
-						     <ul> 
-							  <li> De top 3 bij afloop van de competitie ontvangt een beker die wordt gefinancierd met het opgehaalde geld. </li>
-                              <li> Het resterende geld wordt onder de top 5 verdeeld naar verhouding van het aantal gehaalde punten. </li>                              
-                            </ul> 	<br>  
+    <p> Met de bonussen blijft het spannend tot op het einde, je kan hier tot 10 extra punten mee verdienen.
+        Voor de start van het toernooi moet ik van iedereen een voorspelling hebben (via tabblad Gokken --> bonussen) voor onderstaande gebeurtenissen:
+
+        <ul>
+            <li>  Wie wordt wereldkampioen? <b>4</b> punten </li>
+            <li>  Wie is verliezend finalist? <b>2</b> punten </li>
+            <li> Waar eindigen de Belgen? <b>2</b> punten </li>
+            <li>  Wie wordt de vuilste ploeg? <b>2</b> punt</li>
+            <li> Wie wordt topschutter? <b>2</b> punt</li>
+        </ul>
+        Als je finalist blijkt kampioen te worden en/of omgekeerd krijg je ook nog een aantal punten:
+        <ul>
+        <li> Wereldkampioen OF finalist juist, maar op de verkeerde positie: <b>2</b> punten </li>
+        <li> Wereldkampioen EN finalist juist, maar omgewisseld: <b>4</b> punten</li>
+        </ul>
+        Bij wereldkampioen en finalist hetzelfde land invullen is verboden.
+    </p>
+    <br>
+    <h4> Berekening bonussen </h4>
+    Iedereen met het juiste resultaat ontvangt de bonuspunten! <br>
+    <p>
+        <ol>
+            <li> Vuilste ploeg:
+                <ul>
+                    <li> gele kaart = <b>1</b> punt </li>
+                    <li> rode kaart = <b>3</b> punten </li>
+
+                </ul>
+            </li>
+            <br>
+            <li>
+            Positie België:
+                <ul>
+                    <li>finale </li>
+                    <li> halve finale </li>
+                    <li> kwartfinale </li>
+                    <li> 1/8ste finale </li>
+                    <li> groepsfase </li>
+                </ul> <br>
+                Je gokt het verst je denkt dat ze geraken, als je dit correct hebt krijg je 2 punten.
+            </li>
+        </ol>
+    </p>
+    <br>
+
+    <h4> Inzet </h4>
+
+    <p> Iedereen bezorgt mij 10€ voor 14 juni 2018, dit kan cash of via overschrijving:
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                <div class="info-box-4">
+                    <div class="icon">
+                        <i class="material-icons col-light-green">credit_card</i>
+                    </div>
+                    <div class="content">
+                        <div class="text"><h4>Sam Wesemael</h4></div>
+                        <div class="number">BE46 7805 9286 6336</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </p>
+    <h4> Winst </h4>
+
+     Het opgehaalde geld wordt als volgt verdeeld:
+     <ul>
+      <li> De top 3 bij afloop van de competitie ontvangt een beker die wordt gefinancierd met het opgehaalde geld. </li>
+      <li> Het resterende geld wordt onder de top 5 verdeeld naar verhouding van het aantal gehaalde punten. </li>
+    </ul> 	<br>
+</div>
